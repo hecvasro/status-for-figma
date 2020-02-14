88e57ba6230515bbf706f38e32d4281d8f625aab
Héctor Vásquez <h.vasquez@gbh.com.do>
2020-02-14 00:40:50 -0400

    major re-write

--- src/helpers/settings.ts
+++ src/helpers/settings.ts
@@ -1,26 +1,31 @@
-const SETTINGS_KEY = "plugin-settings";
+const STORAGE_KEY = 'hecvr/status/settings';
 
-export interface PluginSettings {
-  shouldGroupBadgeAndElement: boolean;
+export interface SettingsData {
+  statuses: {[key: string]: {name: string, color: RGB}};
 }
 
-export const loadSettings = async (): Promise<PluginSettings> => {
-  const defaults: PluginSettings = {
-    shouldGroupBadgeAndElement: false
-  };
-
-  try {
-    const storedSettings = await figma.clientStorage.getAsync(SETTINGS_KEY);
-
-    return {
-      ...defaults,
-      ...storedSettings
-    };
-  } catch {
-    return defaults;
+const defaults: SettingsData = {
+  statuses: {
+    'in-progress': {name: 'In Progress', color: {r: 251/255, g: 140/255, b: 0}},
+    'pending-review': {name: 'Pending Review', color: {r: 253/255, g: 216/255, b: 53/255}},
+    'changes-requested': {name: 'Changes Requested', color: {r: 229/255, g: 57/255, b: 53/255}},
+    'done': {name: 'Done', color: {r: 67/255, g: 160/255, b: 71/255}}
   }
 };
 
-export const updateSettings = (nextSettings: PluginSettings): Promise<void> => {
-  return figma.clientStorage.setAsync(SETTINGS_KEY, nextSettings);
+const load = async (): Promise<SettingsData> => {
+  return figma.clientStorage.getAsync(STORAGE_KEY)
+                            .then((settings) => {
+                              if (!settings) {
+                                return update(defaults);
+                              }
+                              return settings;
+                            })
 };
+
+const update = async (settings: SettingsData): Promise<SettingsData> => {
+  return figma.clientStorage.setAsync(STORAGE_KEY, settings)
+                            .then(() => settings);
+};
+
+export default { load, update };
---
+++ src/helpers/status.ts
@@ -0,0 +1,122 @@
+import {SettingsData} from './settings';
+
+const DEFAULT_COLOR: RGB = {r: 0, g: 0, b: 0};
+const DEFAULT_FONT_NAME: FontName = {family: 'Roboto', style: 'Regular'};
+
+const PLUGIN_DATA_KEY = 'hecvr/status';
+
+interface InstanceData {
+  status?: string;
+  id?: string;
+}
+
+const getComponent = async (): Promise<ComponentNode> => {
+  const componentId = figma.root.getPluginData(PLUGIN_DATA_KEY);
+  
+  let component = figma.getNodeById(componentId) as ComponentNode;
+  if (component) {
+    return component;
+  }
+
+  component = figma.createComponent();
+  component.resize(320, 588);
+  component.name = 'Status';
+  component.backgrounds = [];
+  component.constraints = {horizontal: 'STRETCH', vertical: 'STRETCH'};
+  component.locked = true;
+
+  // add badge
+  const badgeBackground = figma.createRectangle();
+  badgeBackground.resize(120, 30);
+  badgeBackground.name = 'Status Badge Background';
+  badgeBackground.fills = [{type: 'SOLID', color: DEFAULT_COLOR}];
+  badgeBackground.cornerRadius = 4.0;
+  badgeBackground.bottomLeftRadius = 0;
+  badgeBackground.bottomRightRadius = 0;
+  badgeBackground.constraints = {horizontal: 'MIN', vertical: 'MIN'};
+  
+  await figma.loadFontAsync(DEFAULT_FONT_NAME);
+  const badgeText = figma.createText();
+  badgeText.resize(120, 30);
+  badgeText.name = 'Status Badge Text';
+  badgeText.textAlignHorizontal = 'CENTER';
+  badgeText.textAlignVertical = 'CENTER';
+  badgeText.fills = [{type: 'SOLID', color: {r: 1, g: 1, b: 1}}];
+  badgeText.constraints = {horizontal: 'MIN', vertical: 'MIN'}
+
+  const badge = figma.group([badgeBackground, badgeText], component);
+  badge.resize(120, 30);
+  badge.name = 'Status Badge';
+
+  component.appendChild(badge);
+
+  // add borders
+  const borders = figma.createRectangle();
+  borders.resize(320, 558);
+  borders.name = 'Status Borders';
+  borders.y = 30;
+  borders.fills = [];
+  borders.strokes = [{type: 'SOLID', color: DEFAULT_COLOR}];
+  borders.strokeAlign = 'INSIDE';
+  borders.strokeWeight = 10;
+  borders.cornerRadius = 4.0;
+  borders.topLeftRadius = 0;
+  borders.constraints = {horizontal: 'STRETCH', vertical: 'STRETCH'};
+
+  component.appendChild(borders);
+
+  // set plugin data for root
+  figma.root.setPluginData(PLUGIN_DATA_KEY, component.id);
+
+  return component;
+};
+
+const updateInstance = async (settings: SettingsData, status: string, instance: InstanceNode): Promise<void> => {
+  const {name, color} = settings.statuses[status];
+
+  // update badge
+  const badge = instance.findOne((node) => node.name === 'Status Badge') as GroupNode;
+
+  const badgeBackground = badge.findOne((node) => node.name === 'Status Badge Background') as RectangleNode;
+  badgeBackground.fills = [{type: 'SOLID', color}];
+
+  await figma.loadFontAsync(DEFAULT_FONT_NAME);
+  const badgeText = badge.findOne((node) => node.name === 'Status Badge Text') as TextNode;
+  badgeText.characters = name;
+
+  // update borders
+  const borders = instance.findOne((node) => node.name === 'Status Borders') as RectangleNode;
+  borders.strokes = [{type: 'SOLID', color}];
+};
+
+const set = async (settings: SettingsData, status: string, node: SceneNode): Promise<void> => {
+  const rawData = node.getPluginData(PLUGIN_DATA_KEY);
+  let data: InstanceData = {};
+  if (rawData) {
+    data = JSON.parse(rawData) as InstanceData;
+  }
+
+  let instance = figma.getNodeById(data.id) as InstanceNode;
+  if (instance) {
+    // update component instance
+    await updateInstance(settings, status, instance);
+  } else {
+    const component = await getComponent();
+
+    // create instance
+    instance = component.createInstance();
+    instance.resize(node.width + 20, node.height + 50);
+    instance.x = node.x - 10;
+    instance.y = node.y - 40;
+    // update component instance
+    await updateInstance(settings, status, instance);
+
+    // add component instance to node's parent
+    (node.parent || figma.currentPage).appendChild(instance);
+  }
+
+  // set plugin data for node
+  node.setPluginData(PLUGIN_DATA_KEY, JSON.stringify({status, id: instance.id}));
+};
+
+export default { set };
